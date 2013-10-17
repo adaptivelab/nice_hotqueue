7ea465257f9962cf4390f81b923379a75717e0b4
Edward Stone <edward@adaptivelab.co.uk>
2013-10-17 13:46:36 +0100

    log once when backing off and once when starting up again

--- nice_hotqueue/__init__.py
+++ nice_hotqueue/__init__.py
@@ -1,4 +1,10 @@
+import logging
 import time
+
+logger = logging.getLogger('nice_hotqueue')
+# use NullHandler as default to avoid output if no logging config
+# in application using library
+logger.addHandler(logging.NullHandler())
 
 
 class NiceHotQueue(object):
@@ -9,10 +15,40 @@ class NiceHotQueue(object):
     def __init__(self, queue, max_queue_size=10000):
         self.queue = queue
         self.max_queue_size = max_queue_size
+        self.backing_off = False
+
+    @property
+    def redis_hosts(self):
+        hosts = []
+        pool = (self._HotQueue__redis.
+            connection_pool._available_connections)
+        for conn in pool:
+            # might be a socket connection without a host
+            try:
+                hosts.append(conn.host)
+            except AttributeError:
+                hosts.append(None)
+        return hosts
+
+    @property
+    def queue_name_and_hosts(self):
+        return 'hotqueue:{} on {}'.format(
+            self.queue.name, ','.join(self.redis_hosts))
 
     def put(self, *args):
         while self.queueIsTooFull():
+            if not self.backing_off:
+                msg = 'backing off {} (queue size: {})'.format(
+                    self.queue_name_and_hosts, len(self.queue))
+                logger.info(msg)
+                self.backing_off = True
             self.backOffABit()
+
+        if self.backing_off:
+            msg = 'inserting into {} again (queue size: {})'.format(
+                self.queue_name_and_hosts, len(self.queue))
+            logger.info(msg)
+            self.backing_off = False
 
         self.queue.put(*args)
 
--- nice_hotqueue/test/test_nice_hotqueue.py
+++ nice_hotqueue/test/test_nice_hotqueue.py
@@ -3,7 +3,24 @@ import fudge
 from nice_hotqueue import NiceHotQueue
 
 
-class FakeFullQueue(object):
+class FakeBaseQueue(object):
+
+    @property
+    def name(self):
+        return 'nicey'
+
+    @property
+    def _HotQueue__redis(self):
+        return fudge.Fake('_HotQueue__redis').has_attr(
+            connection_pool=fudge.Fake('connection_pool').has_attr(
+                _available_connections=[
+                    fudge.Fake('conn').has_attr(host='127.0.0.1')
+                ]
+            )
+        )
+
+
+class FakeFullQueue(FakeBaseQueue):
 
     def __init__(self):
         self.size = 10002
@@ -13,16 +30,25 @@ class FakeFullQueue(object):
         return self.size
 
 
-class FakeEmptyQueue(object):
+class FakeStaysFullQueue(FakeBaseQueue):
+
+    def __init__(self):
+        self.size = 10001
+
+    def __len__(self):
+        return self.size
+
+
+class FakeEmptyQueue(FakeBaseQueue):
 
     def __len__(self):
         return 0
 
 
-class FakeSmallQueue(object):
+class FakeSmallQueue(FakeBaseQueue):
 
     def __init__(self):
-        self.size = 3
+        self.size = 4
 
     def __len__(self):
         self.size -= 1
@@ -30,6 +56,50 @@ class FakeSmallQueue(object):
 
 
 class TestNiceHotQueue(unittest.TestCase):
+
+    @fudge.patch('nice_hotqueue.logger')
+    def test_queue_only_logs_once_when_backing_off(self, logger):
+        (logger
+            .expects('info')
+            .with_args(
+                'backing off hotqueue:nicey on '
+                '127.0.0.1 (queue size: 10001)')
+            .next_call()
+            .with_args(
+                'inserting into hotqueue:nicey on '
+                '127.0.0.1 again (queue size: 10001)')
+        )
+        fake_queue = FakeStaysFullQueue()
+        fake_queue.put = fudge.Fake('put').is_callable().expects_call()
+
+        queue = NiceHotQueue(fake_queue)
+        queue.backOffABit = (
+            fudge.Fake('backOffABit')
+                .is_callable()
+                .expects_call()
+                .times_called(3)
+            )
+        queue.queueIsTooFull = (
+            fudge.Fake('queueIsTooFull')
+                .is_callable()
+                .returns(True)
+                .next_call()
+                .returns(True)
+                .next_call()
+                .returns(True)
+                .next_call()
+                .returns(False))
+        queue.put('something')
+
+
+    @fudge.test
+    def test_queue_name_and_hosts(self):
+        fake_queue = FakeSmallQueue()
+        queue = NiceHotQueue(fake_queue)
+        self.assertEqual(
+            queue.queue_name_and_hosts,
+            'hotqueue:nicey on 127.0.0.1'
+        )
 
     @fudge.test
     def test_put_sleeps_if_the_queue_is_too_full(self):
